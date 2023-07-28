a3f690ce0be90716739b47dd7dd75f0e969c0bcb
BhushT <thavkarbhushan@gmail.com>
2023-07-29 01:08:29 +0530

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const axios = require('axios')
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.sendFile("index.html", { root: path.join(__dirname) })
